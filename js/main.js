8cad92b430687328f2402eaee191de92afcc83a2
Thyme <286827770@qq.com>
2018-12-20 15:38:20 +0800

    实现继承父类的类

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-var Class = function(){
-    var klass  = function(){
+var Class = function(parent){
+    var klass = function(){
         this.init.apply(this,arguments);
-    }
-    klass.prototype.init = function(){}
-    return klass;
-}
+    };
+    if(parent){
+        var subclass = function(){};
+        subclass.prototype = parent.prototype;
+        klass.prototype = new subclass;
+    };
 
-var Person = new Class;
-Person.prototype.init = function(){
-    //初始化
+    klass.prototype.init = function(){};
+
+    //定义别名
+    klass.fn = klass.prototype;
+    klass.fn.parent = klass;
+
+    klass._super = klass.__proto__;
+    /*include/extend代码*/
+    klass.include = function(obj){
+        var included = obj.included;
+        for(var i in obj){
+            klass.fn[i] = obj[i];
+        }
+        if(included) included(klass);
+    };
+    klass.extend = function(obj){
+        var extended = obj.exdended;
+        for(var i in obj){
+            klass[i] = obj[i];
+        }
+        if(extended) extended(klass);
+    }
+    return klass; 
 }
-var person = new Person;
